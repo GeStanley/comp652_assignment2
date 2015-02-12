bb416f4d1e0080d3610c74e136ea7c6c1ae9ab46
GeStanley <geoffrey.stanley@gmail.com>
2015-02-12 13:55:08 -0500

    finished gaussian with full matrix

--- assignment2.py
+++ assignment2.py
@@ -15,6 +15,7 @@ def five_fold_cross_validation(feature_array, target_vector):
 
     five_folds = numpy.array_split(complete_array, 5)
 
+    returned_statistics = {}
     # go through the 5 training cases
     for i in range(0, 5):
         training_data = numpy.empty((0, complete_array.shape[1]), dtype=float)
@@ -38,51 +39,64 @@ def five_fold_cross_validation(feature_array, target_vector):
         testing_features = testing_data[:, 0:testing_data.shape[1]-1]
         testing_target = testing_data[:, testing_data.shape[1]-1]
 
-        print logReg.predict_proba(testing_features)
-        print logReg.predict(testing_features)
-        print testing_target
-        print
+        training_average = numpy.average(logReg.predict_proba(training_features), axis=0)
+        testing_average = numpy.average(logReg.predict_proba(testing_features), axis=0)
 
-def calculate_sigmoid_hypothesis(weights, feature_vector):
+        training_accurate = numpy.sum(logReg.predict(training_features) == training_target)
+        training_accuracy = float(training_accurate) / float(training_target.size)
 
-    exponent = numpy.dot(weights.T, feature_vector)
+        testing_accurate = numpy.sum(logReg.predict(testing_features) == testing_target)
+        testing_accuracy = float(testing_accurate) / float(testing_target.size)
 
-    return 1/(1+math.exp(exponent))
+        fold_data = {'log L train': training_average,
+                     'log L test': testing_average,
+                     'training accuracy': training_accuracy,
+                     'testing accuracy': testing_accuracy}
 
-def calculate_sigmoid_hypothesis_vector(weights, feature_array):
-
-    vector = numpy.empty(0, dtype=float)
-
-    for i in range(0, feature_array.shape[0]):
-        hypothesis = calculate_sigmoid_hypothesis(weights, feature_array[i])
-
-        vector = numpy.append(vector, [hypothesis], axis=0)
+        returned_statistics[i] = fold_data
 
 
-    return vector
-
-def calculate_loss(feature_array, target_vector, weights):
-
-    hypothesis_vector = calculate_sigmoid_hypothesis_vector(weights, feature_array)
-
-    error = hypothesis_vector - target_vector
-    gradient = numpy.dot(feature_array.T, error)
-
-    return gradient
-
-def logistic_regression(feature_array, target_vector, learning_rate=0.001, precision=0.000001):
-
-    weights = numpy.ones(feature_array.shape[1], dtype=float)
-
-    step = learning_rate * calculate_loss(feature_array, target_vector, weights)
-
-    while abs(numpy.sum(step)) > precision:
-
-        weights += step
-        step = learning_rate * calculate_loss(feature_array, target_vector, weights)
-
-
-    return weights
+    return returned_statistics
+# def calculate_sigmoid_hypothesis(weights, feature_vector):
+#
+#     exponent = numpy.dot(weights.T, feature_vector)
+#
+#     return 1/(1+math.exp(exponent))
+#
+# def calculate_sigmoid_hypothesis_vector(weights, feature_array):
+#
+#     vector = numpy.empty(0, dtype=float)
+#
+#     for i in range(0, feature_array.shape[0]):
+#         hypothesis = calculate_sigmoid_hypothesis(weights, feature_array[i])
+#
+#         vector = numpy.append(vector, [hypothesis], axis=0)
+#
+#
+#     return vector
+#
+# def calculate_loss(feature_array, target_vector, weights):
+#
+#     hypothesis_vector = calculate_sigmoid_hypothesis_vector(weights, feature_array)
+#
+#     error = hypothesis_vector - target_vector
+#     gradient = numpy.dot(feature_array.T, error)
+#
+#     return gradient
+#
+# def logistic_regression(feature_array, target_vector, learning_rate=0.001, precision=0.000001):
+#
+#     weights = numpy.ones(feature_array.shape[1], dtype=float)
+#
+#     step = learning_rate * calculate_loss(feature_array, target_vector, weights)
+#
+#     while abs(numpy.sum(step)) > precision:
+#
+#         weights += step
+#         step = learning_rate * calculate_loss(feature_array, target_vector, weights)
+#
+#
+#     return weights
 
 def covariance(vector_a, vector_b):
 
@@ -96,6 +110,35 @@ def covariance(vector_a, vector_b):
 
     return numpy.dot(error_a, error_b.T) / N
 
+def buildCovarianceMatrix(feature_array, type='full'):
+
+    if type == 'full':
+        return 1
+    elif type == 'diagonal':
+        return 0
+    else:
+        return 'error'
+
+def gaussian_multivariate_exponent_calculation(x_vector, x_mean, covariance_matrix):
+
+    difference = x_vector - x_mean
+
+    inverse = numpy.linalg.inv(covariance_matrix)
+
+    dot = numpy.dot(difference.T, inverse)
+
+    calculation = numpy.dot(dot, difference)
+
+    return numpy.exp(1.0/2.0 * calculation)
+
+def gaussian_multivariate_denominator_calculation(n, covariance_matrix):
+
+    determinant = numpy.linalg.det(covariance_matrix)
+
+    root = ((2 * numpy.pi) ** n) * determinant
+
+    return numpy.sqrt(root)
+
 def gaussian_discriminant_analysis(feature_array, target_vector):
     #p(x |  y = 0 ) = 1/(2*pie)
 
@@ -106,14 +149,25 @@ def gaussian_discriminant_analysis(feature_array, target_vector):
     cov_matrix = 0
 
     for y in range(0, 2):
-        sub_set = data_set[data_set[:, data_set.shape[1] - 1] == y]
+        sub_set = data_set[data_set[:, data_set.shape[1] - 1] == y, :-1]
 
         mean = numpy.mean(sub_set, axis=0)
 
-        for i in range(0, data_set.shape[0]-1):
-            row = data_set[:, i]
+        numpy.savetxt('subsetT.out', sub_set.T, delimiter=',')
 
+        covariance_matrix = numpy.cov(sub_set.T)
 
+        n = data_set.shape[0]
+
+        for i in range(0, n-1):
+            row = data_set[i, :-1]
+
+            exponent = gaussian_multivariate_exponent_calculation(row, mean, covariance_matrix)
+            denominator = gaussian_multivariate_denominator_calculation(n, covariance_matrix)
+
+            model_output[i, y] = (1/denominator) * exponent
+
+    return model_output
 
 
 if __name__ == '__main__':
@@ -121,27 +175,23 @@ if __name__ == '__main__':
     array_x = numpy.loadtxt('wpbcx.dat', float)
     vector_y = numpy.loadtxt('wpbcy.dat', float)
 
-    # gaussian_discriminant_analysis(array_x, vector_y)
+    stats = gaussian_discriminant_analysis(array_x, vector_y)
+    #stats = five_fold_cross_validation(array_x, vector_y)
+
+    print stats
+
+    #weights = logistic_regression(array_x, vector_y)
+
+
+    # logReg = LogisticRegression()
+    # logReg.fit(array_x, vector_y)
     #
-    plt.scatter(array_x[:, 10], array_x[:, 2], c=vector_y)
-    plt.show()
-
-
-    #five_fold_cross_validation(array_x, vector_y)
-
-
-    weights = logistic_regression(array_x, vector_y)
-
-
-    logReg = LogisticRegression()
-    logReg.fit(array_x, vector_y)
-
-    print weights
-    print logReg.coef_
-    # for i in range(0, array_x.shape[1]):
-    #     print calculate_sigmoid_hypothesis(weights, array_x[i,])
-
-   # print logReg.predict_proba(array_x)
+    # #print weights
+    # print logReg.coef_
+    # # for i in range(0, array_x.shape[1]):
+    # #     print calculate_sigmoid_hypothesis(weights, array_x[i,])
+    #
+    # print logReg.predict_proba(array_x)
 
     # print array_x.shape
     #
--- testing.py
+++ testing.py
@@ -6,39 +6,21 @@ import assignment2
 
 class TestAssignment1(unittest.TestCase):
 
-    def test_covariance(self):
+    def test_gaussian_multivariate_exponent_calculation(self):
 
-        dataset1 = numpy.array([[1,2,3,4,5,6,7,8,9,10]])
-        dataset2 = numpy.array([[10,9,8,7,6,5,4,3,2,1]])
+        row = numpy.array([[1], [2]])
+        row_mean = numpy.array([[2], [2]])
+        covariance_matrix = numpy.array([[11.71, -4.286], [-4.286, 2.144]])
 
-
-        expected = numpy.cov(dataset1, dataset2)
-
-
-        result = assignment2.covariance(dataset1, dataset2)
-
-        print expected
-        print result
-
-    # def test_build_normalized_array(self):
-    #
-    #     dataset = numpy.array([[2, -10, 1],
-    #                             [4, 6, 1],
-    #                             [10, 8, 1],
-    #                             [-8, 7, 1],
-    #                             [8, -5, 1]], dtype=float)
-    #
-    #     result = load_data.BuildNormalizedArray(dataset)
-    #
-    #     expected_result = numpy.array([[0.2, -1, 1],
-    #                             [0.4, 0.6, 1],
-    #                             [1, 0.8, 1],
-    #                             [-0.8, 0.7, 1],
-    #                             [0.8, -0.5, 1]])
-    #
-    #     self.assertTrue(numpy.allclose(result, expected_result, rtol=1e-01, atol=1e-01))
+        result = assignment2.gaussian_multivariate_exponent_calculation(row, row_mean, covariance_matrix)
 
 
+        expected = 1.1724955240019641
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestAssignment1)
-unittest.TextTestRunner(verbosity=2).run(suite)
+        self.assertEquals(result[0, 0], expected)
+
+
+if __name__ == '__main__':
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAssignment1)
+    unittest.TextTestRunner(verbosity=2).run(suite)
